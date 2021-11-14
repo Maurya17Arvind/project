32e28c9a6295a326b0001e4e7cff33ec23061c56
Arvind maurya <70002646+Maurya17Arvind@users.noreply.github.com>
2021-11-14 14:09:21 +0530

    Add files via upload
    uploded new file by Arvind

---
+++ Project/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" type="text/css" href="css/home.css" >
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+	<body>
+		<section class="header">
+		<nav>
+			<div class="nav-links">
+			<ul>
+				<li><a href="">HOME</a></li>
+				<li><a href="">COURSES</a></li>
+				<li><a href="">FAQs</a></li>
+				<li><a href="">CONTACT US</a></li>
+                <li><a href="">ABOUT US</a></li>
+                <li class="login"><a href="">REGISTER</a></li>
+				<li class="login-1"><a href="">LOGIN</a></li>
+			</ul>
+        </nav>
+		</section>
+        <div>
+        <footer class="panel-footer">
+	<div class="container">
+		<div class="row">
+			<section id="hours" class="col-sm-4">
+				<span>Hours:</span><br>
+				Sun-Thurs: 11:15am - 10:00pm<br>
+				Fri: 11:15am - 2:30pm<br>
+				Saturday Closed
+			</section>
+			<section id="address" class="col-sm-4">
+				<span>Address:</span><br>
+				Althan VIP Road<br>
+				Surat 395454
+				<p>*Dilevery area within 3-4 miles, with minimum order of ₹101 plus ₹10 charge for all deliveries.</p>
+				
+			</section>
+			<section id="testimonials" class="col-sm-4">
+				<p>"The best Indian restaurant I've been to! And that's saying a lot, since I've been to! many!"</p>
+				<p>"Amazing food! Great service! Couldn't ask for more! I'll be back again and again!"</p>
+			
+			</section>
+           
+		</div>
+		 <div class="text-center">
+            	&copy; Copyright Mohhala's Restaurant 2021
+            </div>
+	</div>
+</footer>
+        </div>
+</body>
+</html>
